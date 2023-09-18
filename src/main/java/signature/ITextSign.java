ef0825788476563d92d3a3406c36a901e0b822c7
Zen.Liu <lcz20@163.com>
2023-09-18 12:56:51 +0800

    better kw search

--- src/main/java/signature/ITextSign.java
+++ src/main/java/signature/ITextSign.java
@@ -11,17 +11,15 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.canvas.parser.PdfDocumentContentParser;
 import com.itextpdf.kernel.pdf.canvas.parser.listener.RegexBasedLocationExtractionStrategy;
 import com.itextpdf.layout.Canvas;
-import com.itextpdf.layout.borders.RidgeBorder;
 import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import io.netty.buffer.*;
 import reactor.core.publisher.Mono;
 import reactor.util.annotation.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Zen.Liu
@@ -50,11 +48,11 @@ public class ITextSign implements SignatureService {
     record ImageKeyword(
             ByteBuf image,
             String keyword,
-            int offset
-    ) implements Operate {
+            int offset,
+            float opacity
+    ) implements Keyword {
         @Override
-        public void process(PdfDocument doc, PdfPage page, Canvas canvas) throws Exception {
-            var rect = findLastKeyword(doc, doc.getPageNumber(page), keyword).orElseThrow(() -> new IllegalStateException("not found keyword at last page: " + keyword));
+        public void process(PdfDocument doc, PdfPage page, Rectangle rect, Canvas canvas) throws Exception {
             if (log.isDebugEnabled()) {
                 log.info("rect for {} {}", this, dumpRect(rect));
             }
@@ -62,6 +60,9 @@ public class ITextSign implements SignatureService {
             data.setInverted(true);
             var image = new Image(data);
             image.setFixedPosition(rect.getRight(), rect.getTop() - image.getImageHeight() + offset, image.getWidth());
+            if (opacity > 0 && opacity < 1) {
+                image.setOpacity(opacity);
+            }
             canvas.add(image);
             this.image.release();
         }
@@ -73,15 +74,13 @@ public class ITextSign implements SignatureService {
             String keyword,
             String font,
             float size
-    ) implements Operate {
+    ) implements Keyword {
         @Override
-        public void process(PdfDocument doc, PdfPage page, Canvas canvas) throws Exception {
-            var rect = findLastKeyword(doc, doc.getPageNumber(page), keyword).orElseThrow(() -> new IllegalStateException("not found keyword at last page: " + keyword));
+        public void process(PdfDocument doc, PdfPage page, Rectangle rect, Canvas canvas) throws Exception {
             if (log.isDebugEnabled()) {
                 log.info("rect for {} {}", this, dumpRect(rect));
             }
-            var font = PdfFontFactory.createFont(this.font, "UniGB-UCS2-H", doc);
-            canvas.setBorder(new RidgeBorder(5));
+            var font = PdfFontFactory.createRegisteredFont(this.font);
             canvas.add(new Paragraph(this.text)
                     .setFixedPosition(rect.getRight(), rect.getBottom(), page.getPageSize().getWidth())
                     .setFont(font)
@@ -101,22 +100,29 @@ public class ITextSign implements SignatureService {
         return Optional.of(location.get(location.size() - 1).getRectangle());
     }
 
-/*    record TextLocate(
-            ByteBuf image,
-            float x,
-            float y
-    ) implements Operate {
-        @Override
-        public void process(int maxPage, PdfReader reader, PdfStamper stamper) throws Exception {
-            var image = Image.getInstance(ByteBufUtil.getBytes(this.image));
-            var content = stamper.getOverContent(maxPage);
-            content.addImage(image, image.getScaledWidth(), 0, 0, image.getScaledHeight(), x, y);
+
+    public ITextSign() {
+        PdfFontFactory.registerSystemDirectories();
+        var f = Paths.get("./fonts").toAbsolutePath();
+        if (f.toFile().exists()) {
+            PdfFontFactory.registerDirectory(f.toString());
+            log.info("fonts: {}", PdfFontFactory.getRegisteredFonts());
         }
-    }*/
+    }
 
     interface Operate {
         void process(PdfDocument doc, PdfPage page, Canvas canvas) throws Exception;
 
+    }
+
+    interface Keyword extends Operate {
+        String keyword();
+
+        default void process(PdfDocument doc, PdfPage page, Canvas canvas) throws Exception {
+            throw new IllegalArgumentException();
+        }
+
+        void process(PdfDocument doc, PdfPage page, Rectangle rect, Canvas canvas) throws Exception;
     }
 
     public Mono<ByteBuf> sign(ByteBuf file, List<Operate> operates) {
@@ -126,9 +132,24 @@ public class ITextSign implements SignatureService {
             var writer = new PdfWriter(os);
             try (var doc = new PdfDocument(reader, writer)) {
                 var lastPage = doc.getLastPage();
+                var keywords = operates.stream()
+                        .filter(x -> x instanceof Keyword)
+                        .map(x -> {
+                            var kw = ((Keyword) x).keyword();
+                            var rect = findLastKeyword(doc, doc.getPageNumber(lastPage), kw).orElseThrow(() -> new IllegalStateException("not found keyword " + kw + " at last page"));
+                            return new AbstractMap.SimpleImmutableEntry<>(x, rect);
+                        })
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+
                 try (var canvas = new Canvas(lastPage, new Rectangle(0, 0, lastPage.getPageSize().getWidth(), lastPage.getPageSize().getHeight()))) {
                     for (var op : operates) {
-                        op.process(doc, lastPage, canvas);
+                        if (keywords.containsKey(op)) {
+                            ((Keyword) op).process(doc, lastPage, keywords.get(op), canvas);
+                        } else {
+                            op.process(doc, lastPage, canvas);
+                        }
+
                     }
                 }
 
@@ -142,18 +163,21 @@ public class ITextSign implements SignatureService {
     @Override
     public Mono<ByteBuf> sign(ByteBuf file, @Nullable ByteBuf sign, @Nullable String signKeyword, @Nullable ByteBuf seal, @Nullable String sealKeyword, @Nullable String date, @Nullable String dateKeyword, @Nullable Map<String, String> forms) {
         var ops = new ArrayList<Operate>();
+
         if (forms != null && !forms.isEmpty()) {
             ops.add(new Form(forms));
         }
+        if (date != null && dateKeyword != null) {
+            ops.add(new TextKeyword(date, dateKeyword, "STSONG", 12));
+        }
+
         if (sign != null && signKeyword != null) {
-            ops.add(new ImageKeyword(sign, signKeyword, 0));
+            ops.add(new ImageKeyword(sign, signKeyword, 0, 1));
         }
         if (seal != null && sealKeyword != null) {
-            ops.add(new ImageKeyword(seal, sealKeyword, 30));
+            ops.add(new ImageKeyword(seal, sealKeyword, 30, 0.9f));
         }
-        if (date != null && dateKeyword != null) {
-            ops.add(new TextKeyword(date, dateKeyword, "STSong-Light", 12));
-        }
+
         try {
             return sign(file, ops);
         } finally {
